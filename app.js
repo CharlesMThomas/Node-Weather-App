da3caae280f8245fd14025ede2d55faca4a0b81e
Charles Thomas <charles@charlesmthomas.com>
2017-07-10 15:33:57 -0400

    Print Lat and Lng from Google Geocode API

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=6010%20Drexel%20Lane%20Fort%20Myers',
+    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=515%20Parkside%20St%20Lehigh%20Acres',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lon: ${body.results[0].geometry.location.lng}`);
 });
